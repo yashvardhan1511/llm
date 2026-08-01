5ef3dd6d50982986142ef6f47b943be0e1c7ff7a
Yashvardhan J <jalalpuram6375@gmail.com>
2026-08-01 11:53:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+
+from google import genai
+import os
+from google.genai import types
+import gradio as gr
+
+client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+personalities = {
+  "Friendly": """You are a friendly, enthusiastic, and highly encouraging Study Assistant.
+  Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+  Use analogies and real-world examples that beginners can relate to.
+  Always ask a follow-up question to check understanding.make it concise""",
+  "Academic": """You are a strictly academic, highly detailed, and professional
+  university Professor. Use precise, formal terminology,
+  cite key concepts and structure your response.
+  Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+  Use analogies and real-world examples that beginners can relate to.
+  Always ask a follow-up question to check understanding.make it concise"""
+}
+
+def study_assistant(question, persona):
+
+  system_prompt = personalities[persona]
+
+  response = client.models.generate_content(
+      model = "gemini-3.5-flash",
+      config = types.GenerateContentConfig(
+          system_instruction= system_prompt,
+          temperature = 0.7,
+          max_output_tokens = 1000
+      ),
+      contents = question
+  )
+  return response.text
+demo = gr.Interface(
+    fn=study_assistant,
+    inputs=[
+        gr.Textbox(lines=4, placeholder="Ask a question...", label="Question"),
+        gr.Radio(choices=list(personalities.keys()), value="Friendly", label="Personality")
+    ],
+    outputs=gr.Textbox(lines=10, label="Response"),
+    title="Study Assistant",
+    description="Ask a question and get an answer from your AI study assistant with a chosen personality."
+)
+# change 3 from debug = True ---> server_name and server_port
+demo.launch(
+    server_name = " 0.0.0.0",
+    server_port = int(os.eniron.get("port", 7860))
+)
